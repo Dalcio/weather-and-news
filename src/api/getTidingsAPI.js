8154ce3669701a1180bb57c5e31d459266ac1d31
Dálcio Macuete Garcia <dalciomacuetegarcia@gmail.com>
2021-12-29 11:24:14 -0800

    Update getTidingsAPI.js

--- src/api/getTidingsAPI.js
+++ src/api/getTidingsAPI.js
@@ -3,12 +3,14 @@ import { newsBaseUrl } from "./urls";
 const getTidingsAPI = async (pageSize = 5) =>
   new Promise(async (resolve, reject) => {
     try {
-      const url = new URL(newsBaseUrl);
+      const url = new URL(`https://newsapi.org/v2/top-headlines?category=science&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`);
       url.searchParams.append("pageSize", pageSize);
 
       const response = await fetch(url);
       const data = await response.json();
-
+      
+      console.log("data", data);
+      
       resolve(data);
     } catch (error) {
       reject(undefined);
